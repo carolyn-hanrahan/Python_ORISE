fa09d325e01dbe77d9b940bba7dc0587d4a28314
Carolyn Hanrahan <carolyn.hanrahan23@gmail.com>
2024-11-26 16:42:42 -0500

    update

--- functions.py
+++ functions.py
@@ -53,3 +53,96 @@ def logfc(exp1,exp2,sigdig=3):
 
 #print(logfc(89,12,5))
 #print(logfc(89,12))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ practice ~~~~~~~~~~~~~~
+
+# write a function to calculate fibonacci numbers
+# given a maximum value!.
+
+def fib_function(Fn1, Fn2):
+    print("Fn equals", Fn1+Fn2)
+
+fib_function(4,2)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def fibonacci_below_max(max_value):
+    """
+    Generate a list of Fibonacci numbers below the given maximum value.
+    
+    Parameters:
+        max_value (int): The maximum value for Fibonacci numbers.
+    
+    Returns:
+        list: A list of Fibonacci numbers below max_value.
+    """
+    if max_value <= 0:
+        return []
+    
+    fib_numbers = []
+    a, b = 0, 1
+    while a < max_value:
+        fib_numbers.append(a)
+        a, b = b, a + b
+    
+    return fib_numbers
+
+# Call the function with a maximum value of 100
+print(fibonacci_below_max(100))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Global vs. local variables 
+
+"""
+when a varible is defined outside of a function, 
+it is known as a global variable. a local variable is 
+only accessible within a function. It is generally 
+good programming etiquette to use local variables
+rather than global variables when writing functions
+
+
+"""
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# File handling --> learning how to read from and write files
+
+# I saved a TSV file (tab separated values file)
+
+
+# Reading in a file: 
+print("\nFile Handling\n")
+
+# open the file for reading
+f = open("DMR.GBM2.vs.NB1.tsv", "r")
+print(f.readline())
+print(f.readline())
+
+# you can use a loop to read the whole file, line by line
+f2 = open("DMR.GBM2.vs.NB1.tsv", "r")
+for line in f2:
+    print(line)
+
+# always close file handles when you're done with them
+f.close()
+f2.close()
+
+# Writing a file: 
+
+# open a new file for writing, will create if doesn't exist
+f3 = open("myfile.txt", "w")
+f3.write("Hello World!")
+f3.close()
+
+# read the whole file at once
+f4 = open("myfile.txt","r")
+print(f4.read())
+f4.close()
+
+# Now append to the file
+f3 = open("myfile.txt", "a")
+f3.write("How are you?")
+f3.close()
+
+f4 = open("myfile.txt","r")
+print(f4.read())
+f4.close()
